8d1c1870d17c0e254ec4dcb0040385fa53cb8108
Ilya <koselevicila@gmail.com>
2022-08-25 21:43:02 +0300

    111

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -7,11 +7,11 @@ type InputPropsType={
 
 export const Input = (props:InputPropsType) => {
 
-    const onChangeInoutHandler = (event:ChangeEvent<HTMLInputElement>)=>{
+    const onChangeInputHandler = (event:ChangeEvent<HTMLInputElement>)=>{
         props.setTitle(event.currentTarget.value)
     }
     return (
-        <input value={props.title} onChange={onChangeInoutHandler}/>
+        <input value={props.title} onChange={onChangeInputHandler}/>
     );
 };
 
